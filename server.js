aeebdde0e00b145f9fe1998e1df89c0d7754b1f1
Emil Stausbøl <emil2@live.dk>
2021-11-12 13:31:50 +0100

    Server er sat op

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const express = require("express");
 
 const app = express();
 
-app.use(express.sstatic("public"));
+app.use(express.static("public"));
 app.use(express.json());
 
 //Definere en port
